4f496d1c31163866b4fd92b25f2d6f27f8e600da
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:38:02 +0700

    Add files via upload

---
+++ rata_rata.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+$mahasiswa1 = 9;
+$mahasiswa2 = 10;
+$mahasiswa3 = 9;
+$mahasiswa4 = 10;
+echo "Masukkan Banyaknya Mahasiswa : 4<br>";
+//menjumlahkan nilai rata-rata
+     echo " Nilai Mahasiswa ke-1 : 9<br>";
+     echo " Nilai Mahasiswa ke-2 : 10<br>";
+     echo " Nilai Mahasiswa ke-3 : 9<br>";
+     echo " Nilai Mahasiswa ke-4 : 10<br>";
+$hasil = $mahasiswa1 + $mahasiswa2 + $mahasiswa3 + $mahasiswa4;
+$rata = $hasil / 4;
+echo "maka,Nilai rata-ratanya adalah $rata";
+
+?>
